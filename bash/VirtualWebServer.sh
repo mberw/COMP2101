3a7c5af2361ecc253efb51b2dfa3d0cca8ed9713
mberw <you@yourdomain>
2022-06-26 13:22:01 -0400

    Updates and improvements

--- bash/VirtualWebServer.sh
+++ bash/VirtualWebServer.sh
@@ -50,14 +50,13 @@ else
 fi
 
 
-#checking to see whether we can find apache2 install status and putting it in a variable named $apache2
-apache2=$(dpkg --get-selections | grep apache2)
-
-
-#Checking to see whether apache2 is installed by looking to see if there is a string present
-#in the $apache2 variable created earlier
+#Checking to see whether apache2 is installed in the COMP2101-S22 container and not the host machine
+#by looking to see if there is a string present in the $apache2 variable created below
 #download and install apache2 to the COMP2101-S22 container if apache2 not found and inform the user
 #or inform the user if apache2 is already installed
+apache2=$(lxc exec COMP2101-S22 -- dpkg --get-selections | grep apache2)
+
+
 if [[ -z "$apache2" ]] ; then
 	echo "Checking Apache2 status: ..."
 	echo "Apache2 is not yet installed. Installing now!"
@@ -71,11 +70,26 @@ fi
 
 
 
+
+
 #get the default web page from the apache2 webserver that was installed
-lxc exec COMP2101-S22 -- curl http://127.0.0.1
+#used the head option so that the html isn't being outputted
 
 
-#let the user know whether the script was successful in getting the default Apache2 webpage.
+#see what ip the container is using and put that in a variable named ip
+#this grep is extended and searches only for matched, non empty parts (so there must
+#be something in each octect, 1 to 3 numbers in each octect too)
+ip=$(lxc list | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
+#curl the webpage for apache2 install in the container located at that ip
+# (used head option so that we're not seeing the entire html outputted to the screen)
+lxc exec COMP2101-S22 -- curl --head $ip
+
+
+
+
+#let the user know whether the script was successful in getting the default Apache2 webpage based
+#on whether the last command was successful (if it was successful, we know we were able to 
+#curl the default index file on the apache2 web server.
 if [ "$?" = 0 ] ; then
 	echo "Successfully retrieved default apache2 web page!"
 else
