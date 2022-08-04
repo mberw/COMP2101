a41fb4ed0acf1d7e3579d298a3ee6f9cfb4fc6bb
mberw <96561587+mberw@users.noreply.github.com>
2022-08-04 15:27:28 -0700

    Update ipreport.ps1
    Updated some comments and small improvements to the script.

--- powershell/ipreport.ps1
+++ powershell/ipreport.ps1
@@ -1,11 +1,16 @@
+
+#Let the user know which network adapters are active/enabled on the system
+
 "Currently enabled Network Adapters are:"
 get-ciminstance -class win32_networkadapterconfiguration | ? ipenabled
 
-
+<#Give the user more details about the network adapters listed above. 
+Here we're getting the network adapter class from get-ciminstance that are listed as enabled where the win32_networkadapterconfiguration properties are not equal (-ne) to nothing ($null)
+#>
 "********************"
 "Below is a table containing additional details about the network adapters listed above"
 "********************"
-get-ciminstance -class win32_networkadapterconfiguration | where-object ipenabled | ? {$_.dnsdomain -ne $null -or $_.dnshostname -ne $null -or $_.dnsserversearchorder -ne $null} | select description, dnsserversearchorder, dnsdomain, dnshostname, dns-server, ipaddress, index, ipsubnet | format-table -autosize
+get-ciminstance -class win32_networkadapterconfiguration | where-object ipenabled | ? {$_.dnsdomain -ne $null -or $_.dnshostname -ne $null -or $_.dnsserversearchorder -ne $null} | select description, dnsserversearchorder, dnsdomain, dnshostname, ipaddress, index, ipsubnet | format-table -autosize
 
 
 
